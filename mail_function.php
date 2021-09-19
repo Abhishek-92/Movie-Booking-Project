5fcf447e3223926bbd49b2a2b801af40adfb1159
Abhishek <abhi.odrani@gmail.com>
2021-09-19 14:20:48 +0530

    Initial commit

---
+++ mail_function.php
@@ -0,0 +1,31 @@
+<?php	
+	// include_once(FCPATH.'PHPMailer/src/PHPMailer.php');
+	// include_once(FCPATH.'PHPMailer/src/SMTP.php');
+	// include_once(FCPATH.'PHPMailer/src/Exception.php');
+	function sendOTP($email,$otp) {
+		// require('phpmailer/class.phpmailer.php');
+		// require('phpmailer/class.smtp.php');
+		require 'PHPMailer-master/PHPMailerAutoload.php';
+	
+		$message_body = "One Time Password for PHP login authentication is:<br/><br/>" . $otp;
+		$mail = new PHPMailer();
+		$mail->IsSMTP();
+		$mail->SMTPDebug = 0;
+		$mail->SMTPAuth = TRUE;
+		$mail->SMTPSecure = 'tls'; // tls or ssl
+		$mail->Port     = "SMTP port";
+		$mail->Username = "SMTP username";
+		$mail->Password = "SMTP Password";
+		$mail->Host     = "SMTP HOST";
+		$mail->Mailer   = "smtp";
+		$mail->SetFrom("FROM EMAIL", "FROM NAME");
+		$mail->AddAddress($email);
+		$mail->Subject = "OTP to Login";
+		$mail->MsgHTML($message_body);
+		$mail->IsHTML(true);		
+		$result = $mail->Send();
+		
+		return $result;
+	}
+?>
+
